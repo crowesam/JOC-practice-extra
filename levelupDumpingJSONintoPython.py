d987822b9fcd13ab51d4b78bf1e793098105e3ee
Sam Crowe <crowesam@users.noreply.github.com>
2024-08-04 12:31:34 -0400

    ving useful commands and code tricks for Python

---
+++ levelupDumpingJSONintoPython.py
@@ -0,0 +1,14 @@
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(f"This is a Python dictionary \n"+ "=" * 15 + " \n" + y)
